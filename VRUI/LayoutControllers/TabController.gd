5cf45bf92088e55e05328079899772625ecf903b
iffn <iffn1990@gmail.com>
2026-08-21 13:40:51 +0200

    Implemented tab controller

---
+++ VRUI/LayoutControllers/TabController.gd
@@ -0,0 +1,19 @@
+extends Node
+
+@export var buttons : Array[Button]
+@export var elements : Array[Control]
+
+@export var default : Button
+
+func _ready() -> void:
+	for button in buttons:
+		button.pressed.connect(select.bind(button))
+	
+	select(default)
+
+func select(button: Button):
+	
+	for i in buttons.size():
+		var is_selected := buttons[i] == button
+		buttons[i].button_pressed = is_selected
+		elements[i].visible = is_selected
